a3c0c52e324a21b7ef388d8e8f7d44a301da952b
kostasdak <kostas_dak@yahoo.com>
2022-04-10 09:49:57 -0400

    latest

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -42,20 +42,29 @@ func main() {
 // App handler ... Builds the structure of the app !!!
 func AppHandler(db *sql.DB, cfg *gomvc.AppConfig) http.Handler {
 
-	// initialize
+	// initialize controller
 	c.Initialize(db, cfg)
 
 	// load template files ... path : /web/templates
+	// required : homepagefile & template file
+	// see [template names] for details
 	c.CreateTemplateCache("home.view.tmpl", "base.layout.html")
 
-	// *** Start registering url, actions and models ***
-	// home page
+	// *** Start registering urls, actions and models ***
+
+	// RegisterAction(url, next, action, model)
+	// url = url routing path
+	// next = redirect after action complete, use in POST actions if necessary
+	// model = database model object for CRUD operations
+
+	// home page : can have two urls "/" and "/home"
 	c.RegisterAction("/", "", gomvc.ActionView, nil)
 	c.RegisterAction("/home", "", gomvc.ActionView, nil)
 
-	// create view model for [products] table, use this model only for view data
-	pViewModel := gomvc.Model{DB: db, IdField: "id", TableName: "products"}
-	pViewModel.AddRelation(db, "colors", "id", "product_id", gomvc.ModelJoinLeft)
+	// create model for [products] database table
+	// use the same model for all action in this example
+	pViewModel := gomvc.Model{DB: db, PKField: "id", TableName: "products", OrderString: "ORDER BY id DESC"}
+	pViewModel.AddRelation(db, "colors", "id", "product_id", gomvc.ModelJoinLeft, gomvc.ResultStyleSubresult)
 
 	// optional assign labels for each table field. Can be used in template view code
 	pViewModel.AssignLabels(map[string]string{
@@ -72,32 +81,40 @@ func AppHandler(db *sql.DB, cfg *gomvc.AppConfig) http.Handler {
 		"colors.color":      "Color", //Related field in table [colors]
 	})
 
-	// view products
+	// view products ... / show all products || /products/view/{id} for one product
 	c.RegisterAction("/products", "", gomvc.ActionView, &pViewModel)
 	c.RegisterAction("/products/view/*", "", gomvc.ActionView, &pViewModel)
 
-	// create edit model for table [products] -> used for create, update, delete only
-	pEditModel := gomvc.Model{DB: db, IdField: "id", TableName: "products"}
+	// create edit model for table [products] -> used for create, update, delete actions only
+	pEditModel := gomvc.Model{DB: db, PKField: "id", TableName: "products"}
 
-	// create product
+	// build create product action ... this url has two actions
+	// #1 View page -> empty product form no redirect url (no next url required)
+	// #2 Post form data to create a new record in table [products] -> then redirect to [next] url -> products page
 	c.RegisterAction("/products/create", "", gomvc.ActionView, &pEditModel)
 	c.RegisterAction("/products/create", "/products", gomvc.ActionCreate, &pEditModel)
 
-	// edit product
+	// build edit product actions ... this url has two actions
+	// #1 View page with the product form -> edit form (no next url required)
+	// #2 Post form data to update record in table [products] -> then redirect to [next] url -> products page
 	c.RegisterAction("/products/edit/*", "", gomvc.ActionView, &pEditModel)
 	c.RegisterAction("/products/edit/*", "/products", gomvc.ActionUpdate, &pEditModel)
 
-	// delete product
+	// build delete product actions ... this url has two actions
+	// #1 View page with the product form -> edit form [locked] to confirm detetion (no next url required)
+	// #2 Post form data to delete record in table [products] -> then redirect to [next] url -> products page
 	c.RegisterAction("/products/delete/*", "", gomvc.ActionView, &pEditModel)
 	c.RegisterAction("/products/delete/*", "/products", gomvc.ActionDelete, &pEditModel)
 
-	// about page
+	// build about page ... static page, no table/model, no [next] url
 	c.RegisterAction("/about", "", gomvc.ActionView, nil)
 
-	// contact page
+	// build contact page ... static page, no table/model, no [next] url
 	c.RegisterAction("/contact", "", gomvc.ActionView, nil)
 
-	// custom action
+	// build contact page POST action ... static page, no table/model, no [next] url
+	// Demostrating how to register a custom func to handle the http request/response using your oun code
+	// and handle POST data and have access to database through the controller and model object
 	c.RegisterCustomAction("/contact", "", gomvc.HttpPOST, nil, ContactPostForm)
 	return c.Router
 }
@@ -105,20 +122,36 @@ func AppHandler(db *sql.DB, cfg *gomvc.AppConfig) http.Handler {
 // Custom handler for specific page and action
 func ContactPostForm(w http.ResponseWriter, r *http.Request) {
 
-	//test ... I have access to products model !!!
-	fmt.Println("**********")
+	//test : I have access to products model !!!
+	fmt.Print("\n\n")
+	fmt.Println("********** ContactPostForm **********")
 	fmt.Println("Table Fields : ", c.Models["/products"].Fields)
 
-	//read data from table products even this is a POST action for contact page
-	rows, _ := c.Models["/products"].GetAllRecords(100)
-	fmt.Println("**********")
-	fmt.Println("Select Rows : ", rows)
+	//read data from table products (Model->products) even if this is a POST action for contact page
+	fmt.Print("\n\n")
+	rows, _ := c.Models["/products"].GetRecords([]gomvc.Filter{}, 100)
+	fmt.Println("Select Rows Example 1 : ", rows)
 
-	//test form Posted fields
-	fmt.Println("**********")
-	fmt.Println("Form Fields : ", r.Form)
+	//read data from table products (Model->products) even if this is a POST action for contact page
+	fmt.Print("\n\n")
+	id, _ := c.Models["/products"].GetLastId()
+	fmt.Println("Select Rows Example 1 : ", id)
 
-	//test session message
+	//read data from table products (Model->products) with filter (id=1)
+	fmt.Print("\n\n")
+	var f = make([]gomvc.Filter, 0)
+	f = append(f, gomvc.Filter{Field: "id", Operator: "=", Value: 1})
+	rows, err := c.Models["/products"].GetRecords(f, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Select Rows Example 2 : ", rows)
+
+	//test : Print Posted Form fields
+	fmt.Print("\n\n")
+	fmt.Println("Form fields : ", r.Form)
+
+	//test : Set session message
 	c.GetSession().Put(r.Context(), "error", "Hello From Session")
 
 	//redirect to homepage
